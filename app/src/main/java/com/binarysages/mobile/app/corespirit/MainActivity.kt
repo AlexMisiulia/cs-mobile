7cd84595fe1606957c829342b0f4626495c7ceac
Sergey Miroshin <miroshinsv1988@yandex.ru>
2019-09-09 12:32:02 +0300

    first imp toolbar

--- app/src/main/java/com/binarysages/mobile/app/corespirit/MainActivity.kt
+++ app/src/main/java/com/binarysages/mobile/app/corespirit/MainActivity.kt
@@ -2,25 +2,39 @@ package com.binarysages.mobile.app.corespirit
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.binarysages.mobile.app.corespirit.adapters.ArticleListAdapter
 import com.binarysages.mobile.app.corespirit.models.ArticleModel
 import com.binarysages.mobile.app.corespirit.network.apiWorks
 
-
 class MainActivity : AppCompatActivity() {
+    private fun toolabar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        find view by id
+        toolabar()
+        
+        //        find view by id
         val articlesRecyclerView: RecyclerView = findViewById(R.id.articlesRecycleViewList)
-//        save manager
+        //        save manager
         val manager = LinearLayoutManager(this)
 //        set layout
         articlesRecyclerView.layoutManager = manager
-//        set what listener must do
+        //        set what listener must do
         val listener = object : ArticleListAdapter.OnArticleClickListener {
             override fun onArticleClick(articleModel: ArticleModel) {
                 val intent = Intent(this@MainActivity, ArticleItemActivity::class.java)
@@ -36,15 +50,16 @@ class MainActivity : AppCompatActivity() {
         articlesRecyclerView.adapter = articleAdapter
 
 //        add on scroll listener for infinity scroll
-        articlesRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (manager.itemCount - 1 == manager.findLastVisibleItemPosition()) {
-                    articleAdapter.addArticles(apiWorks.getArticles())
+        articlesRecyclerView.addOnScrollListener(
+            object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (manager.itemCount - 1 == manager.findLastVisibleItemPosition()) {
+                        articleAdapter.addArticles(apiWorks.getArticles())
 //                    notify adapter that content change
-                    articleAdapter.notifyDataSetChanged()
+                        articleAdapter.notifyDataSetChanged()
+                    }
                 }
-            }
-        })
+            })
     }
 }
