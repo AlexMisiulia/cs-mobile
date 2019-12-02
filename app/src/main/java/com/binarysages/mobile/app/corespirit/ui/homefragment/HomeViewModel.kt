8c605127ddb279f20e27e519dcb963058630eb6a
Sergey Miroshin <miroshinsv1988@yandex.ru>
2019-12-02 15:41:51 +0300

    Remove Log

--- app/src/main/java/com/binarysages/mobile/app/corespirit/ui/homefragment/HomeViewModel.kt
+++ app/src/main/java/com/binarysages/mobile/app/corespirit/ui/homefragment/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.binarysages.mobile.app.corespirit.ui.homefragment
 
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
