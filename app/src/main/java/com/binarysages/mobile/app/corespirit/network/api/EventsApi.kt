865e17462a475eee2e6ed3fbdaeea447e95e58f8
Sergey Miroshin <miroshinsv1988@yandex.ru>
2019-12-04 12:49:47 +0300

    Fix Network request

--- app/src/main/java/com/binarysages/mobile/app/corespirit/network/api/EventsApi.kt
+++ app/src/main/java/com/binarysages/mobile/app/corespirit/network/api/EventsApi.kt
@@ -3,12 +3,11 @@ package com.binarysages.mobile.app.corespirit.network.api
 import com.binarysages.mobile.app.corespirit.models.eventitem.EventModel
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface EventsApi {
-    
-    @GET("events/{eventID}")
+    @GET("events/{eventID}/")
     fun getEventByID(
-        @Query("eventID") eventID: Int
+        @Path("eventID") eventID: Int
     ): Call<EventModel>
 }
